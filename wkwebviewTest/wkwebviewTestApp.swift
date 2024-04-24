103ab4561fc74eaa9bd251927ebfd930fc1b75b0
Luka Lešić <lukalesic20@gmail.com>
2024-04-25 01:49:04 +0200

    Initial Commit

---
+++ wkwebviewTest/wkwebviewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  wkwebviewTestApp.swift
+//  wkwebviewTest
+//
+//  Created by Luka Lešić on 25.04.24.
+//
+
+import SwiftUI
+
+@main
+struct wkwebviewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
